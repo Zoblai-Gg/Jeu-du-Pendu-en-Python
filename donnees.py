4b3641fba5f7d9bf7cf3415fc6023f0b6cd1dd97
zoblai_Gg <blaise.yarbanga@gmail.com>
2024-03-14 10:54:03 +0100

    Les données

---
+++ donnees.py
@@ -0,0 +1,11 @@
+
+""" Ce fichier définit quelques données , sous la forme de variables ,utiles au programme pendu """
+
+# liste de mot à deviner
+liste_de_mots = ["fraise", "banane", "pomme", "raisin", "mangue", "kiwi", "poire", "pèche"]
+
+# Nombre de chance max
+chance = 8
+
+# La taille max d'un mot
+taille_max_mot = 8
